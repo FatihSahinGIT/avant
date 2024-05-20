c0c044d3f2bffc4e88edff5e323a99846b374c5b
Fatih Sahin <s73121@beuth-hochschule.de>
2024-05-20 14:21:48 +0200

    Refacotring detail page

--- assets/js/pages/detail.js
+++ assets/js/pages/detail.js
@@ -11,6 +11,8 @@ fetch("./assets/data/kitchen.json")
 
 function showDetail() {
   let detail = document.querySelector(".detail");
+  let desc = document.querySelector(".desc");
+  let header = document.querySelector(".title");
   let productId = new URLSearchParams(window.location.search).get("id");
 
   let thisProduct = products.find((product) => product.id == productId);
@@ -25,10 +27,32 @@ function showDetail() {
   // Leert das Container-Element für die Bilder
   imageContainer.innerHTML = "";
 
-  // Fügt alle Bilder aus dem Array hinzu
   thisProduct.img.forEach((imgSrc) => {
     let imgElement = document.createElement("img");
     imgElement.src = imgSrc;
     imageContainer.appendChild(imgElement);
   });
+
+  let textElement = document.createElement("p");
+  textElement.innerText = thisProduct.description;
+  desc.appendChild(textElement);
+
+  let headerElement = document.createElement("h1");
+  headerElement.innerText = thisProduct.name;
+  header.appendChild(headerElement);
+
+  // Similar Products
+  let listProduct = document.querySelector(".kitchen-list-similar");
+  products
+    .filter((value) => value.id !== productId)
+    .forEach((product) => {
+      let newProduct = document.createElement("a");
+      newProduct.href = "/detail.html?id=" + product.id;
+      newProduct.classList.add("item");
+      newProduct.innerHTML = `
+      <img src=${product.img[0]} />
+      <h2>${product.name}</h2>
+      `;
+      listProduct.appendChild(newProduct);
+    });
 }
